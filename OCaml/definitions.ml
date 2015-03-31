207f9c6c12ff1dbae61cb7d3c2fd16236bfd1848
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-31 02:25:12 -0400

    adds netrank.ml* files

--- OCaml/definitions.ml
+++ OCaml/definitions.ml
@@ -1,25 +1,26 @@
 type node = string
 type edges = node*(node list)
 type state = Continue | Done
-
-
 type rWalk = node list* state * node
+
 module NodeH = struct
     type t = node
     let hash s1 = Hashtbl.hash s1
-    let equal s1 s2 = String.compare s1 s2 = 0
+    let equal s1 s2 = 
+        (String.compare s1 s2 = 0)
 end
 
 module Network = Hashtbl.Make(NodeH)
-type network = string list Network.t
 
-let module 
-module NodeH = struct
+module W_ijHash = struct
     type t = node * node
     let hash s = let (s1, s2) = s in Hashtbl.hash s1^s2
     let equal a b = 
         let (a1, a2) = a and (b1, b2) = b in 
-        String.compare s1^s2 s2 = 0
+        String.compare (a1^a2) (b1^b2 = 0
 end
+module W_m = Hashtbl.Make(W_ijHash)
+
+type network = string list Network.t
 type w_ij = (node, node)
-type W = int W.t
+type W = int W_m.t
